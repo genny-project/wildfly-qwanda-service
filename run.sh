559874b4ea49a9ae76fa6a725cfbacea5f965d7f
adamcrow64 <adamcrow63@gmail.com>
2017-10-11 14:07:35 +1100

    Updated docker-compose.yml and run.sh

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 ENV_FILE=genny.env
 ./create_genny_env.sh ${ENV_FILE} $1
 echo "HAZELCAST_XML=-Dvertx.hazelcast.config=./cluster.xml" >> ${ENV_FILE}
-#ENV_FILE=$ENV_FILE docker-compose   up -d
-#ENV_FILE=$ENV_FILE docker-compose logs -f qwanda-service 
-docker-compose   up -d
-docker-compose logs -f qwanda-service 
+ENV_FILE=$ENV_FILE docker-compose   up -d
+ENV_FILE=$ENV_FILE docker-compose logs -f qwanda-service 
+#docker-compose   up -d
+#docker-compose logs -f qwanda-service 
