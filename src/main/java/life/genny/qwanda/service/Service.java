0e4245db0826372fda959914c73393380b1edd12
Adam Crow <acrow@Adams-MacBook-Pro.local>
2017-11-15 00:19:19 +1100

    Moved BaseEntityService out of wildfly-qwanda-service into qwanda-services

--- src/main/java/life/genny/qwanda/service/Service.java
+++ src/main/java/life/genny/qwanda/service/Service.java
@@ -1,25 +1,61 @@
 package life.genny.qwanda.service;
 
+import java.io.IOException;
+import java.lang.invoke.MethodHandles;
+import java.time.LocalDateTime;
+
 import javax.annotation.PostConstruct;
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
-import life.genny.daoservices.BaseEntityService;
+
+import org.apache.logging.log4j.Logger;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import life.genny.daoservices.BaseEntityService2;
+import life.genny.qwanda.DateTimeDeserializer;
+import life.genny.qwanda.message.QEventAttributeValueChangeMessage;
 import life.genny.qwanda.util.PersistenceHelper;
+import life.genny.qwandautils.QwandaUtils;
 
 @ApplicationScoped
-public class Service extends BaseEntityService {
+public class Service extends BaseEntityService2 {
 
-  public Service() {
-    // TODO Auto-generated constructor stub
-  }
+	/**
+	 * Stores logger object.
+	 */
+	protected static final Logger log = org.apache.logging.log4j.LogManager
+			.getLogger(MethodHandles.lookup().lookupClass().getCanonicalName());
 
-  @Inject
-  private PersistenceHelper helper;
+	public Service() {
+		// TODO Auto-generated constructor stub
+	}
 
-  @PostConstruct
-  public void init() {
+	@Inject
+	private PersistenceHelper helper;
 
-    this.setEm(helper.getEntityManager());
-  }
+	GsonBuilder gsonBuilder = new GsonBuilder().registerTypeAdapter(LocalDateTime.class, new DateTimeDeserializer());
+	String bridgeApi = System.getenv("REACT_APP_VERTX_SERVICE_API");
 
+	@PostConstruct
+	public void init() {
+
+		this.setEm(helper.getEntityManager());
+	}
+
+	@javax.ejb.Asynchronous
+	public void sendQEventAttributeValueChangeMessage(final QEventAttributeValueChangeMessage event) {
+		// Send a vertx message broadcasting an attribute value Change
+		log.info("ATTRIBUTE CHANGE EVENT!" + event.getTargetBaseEntityCode() + ":" + event.getNewValue());
+		Gson gson = gsonBuilder.create();
+		try {
+			QwandaUtils.apiPostEntity(bridgeApi, gson.toJson(event), event.getToken());
+		} catch (IOException e) {
+
+			log.error(
+					"Error in posting to Vertx bridge:" + event.getTargetBaseEntityCode() + ":" + event.getNewValue());
+		}
+
+	}
 }
---
+++ src/test/java/life/genny/wildfly/BasicTest.java
@@ -0,0 +1,19 @@
+package life.genny.wildfly;
+
+//@RunWith(Arquillian.class)
+public class BasicTest {
+
+	// @Deployment
+	// public static JavaArchive createDeployment() {
+	// return ShrinkWrap.create(JavaArchive.class).addClass(Greeter.class)
+	// .addAsManifestResource(EmptyAsset.INSTANCE, "beans.xml");
+	// }
+	//
+	// @Inject
+	// private Greeter greeter;
+	//
+	// @Test
+	// public void shouldBeAbleTo() {
+	// assertEquals("Hello, aliens!", greeter.createGreeting("aliens"));
+	// }
+}
