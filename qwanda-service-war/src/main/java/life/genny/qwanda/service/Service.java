c3c7bb2ae7388c1ad86ba2fe9bd8817d8704702c
Jian Li <hbjoylee@gmail.com>
2020-09-16 12:17:42 +1000

    use native query

--- qwanda-service-war/src/main/java/life/genny/qwanda/service/Service.java
+++ qwanda-service-war/src/main/java/life/genny/qwanda/service/Service.java
@@ -517,7 +517,7 @@ public class Service extends BaseEntityService2 implements QwandaRepository {
     @Override
     public void cleanAsk(String realm) {
         String qlString= String.format("delete from ask where realm = '%s'", realm);
-        Query query = getEntityManager().createQuery(qlString);
+        Query query = getEntityManager().createNativeQuery(qlString);
         query.executeUpdate();
     }
 
@@ -527,7 +527,7 @@ public class Service extends BaseEntityService2 implements QwandaRepository {
                 "where baseEntityCode like \'RUL_FRM%_GRP\' " +
                 "and attributeCode = \'PRI_ASKS\' " +
                 "and realm = \'" + realm + "\'";
-        Query query = getEntityManager().createQuery(qlString);
+        Query query = getEntityManager().createNativeQuery(qlString);
         query.executeUpdate();
     }
 }
