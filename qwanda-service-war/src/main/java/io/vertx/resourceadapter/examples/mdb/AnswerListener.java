bf09255e6da6bbab1aeb86841ac9254e361461f6
cyberust <you@example.com>
2021-06-22 05:39:35 +0000

    Sending the answer to be saved through kafka

---
+++ qwanda-service-war/src/main/java/io/vertx/resourceadapter/examples/mdb/AnswerListener.java
@@ -0,0 +1,40 @@
+package io.vertx.resourceadapter.examples.mdb;
+
+import java.io.IOException;
+import java.util.concurrent.CompletionStage;
+
+import javax.enterprise.context.ApplicationScoped;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Message;
+
+import io.smallrye.reactive.messaging.annotations.Merge;
+import io.vertx.core.json.JsonObject;
+import life.genny.qwanda.Answer;
+import life.genny.qwandautils.JsonUtils;
+import life.genny.qwandautils.QwandaUtils;
+
+@ApplicationScoped
+public class AnswerListener {
+
+
+	@Incoming("answer")
+	@Merge
+	public CompletionStage<Void> fromAnswers(Message<String>  message) {
+		System.out.println("Here receive from answer channel");
+
+		JsonObject json = new JsonObject(message.getPayload());
+		String token = json.getString("token");
+		json.remove("token");
+		Answer answer = JsonUtils.fromJson(json.toString(),Answer.class);
+
+		try {
+			QwandaUtils.apiPostEntity2("http://localhost:8080/qwanda/answers",
+					JsonUtils.toJson(answer), token, null);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+		return	message.ack();
+	}
+
+}
