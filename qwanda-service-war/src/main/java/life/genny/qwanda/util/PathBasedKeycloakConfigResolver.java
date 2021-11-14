5faa404f296569cd8247217daeddf85d668abf8e
Jian Li <hbjoylee@gmail.com>
2021-11-15 10:55:07 +1100

    disable debug logs

--- qwanda-service-war/src/main/java/life/genny/qwanda/util/PathBasedKeycloakConfigResolver.java
+++ qwanda-service-war/src/main/java/life/genny/qwanda/util/PathBasedKeycloakConfigResolver.java
@@ -47,15 +47,15 @@ public class PathBasedKeycloakConfigResolver implements KeycloakConfigResolver {
 				// Now check for a token
 
 				if (request.getHeader("Authorization") != null) {
-					log.info("request.getHeader(\"Authorization\") is not null!");
+					log.debug("request.getHeader(\"Authorization\") is not null!");
 					// extract the token
 					final String authTokenHeader = request.getHeader("Authorization");
-					log.info("authTokenHeader ="+authTokenHeader);
+					log.debug("authTokenHeader ="+authTokenHeader);
 					final String bearerToken = authTokenHeader.substring(7);
-					log.info("bearerToken ="+bearerToken);
+					log.debug("bearerToken ="+bearerToken);
 					
 					gennyToken = new GennyToken(bearerToken);
-					log.info(gennyToken);
+					log.debug(gennyToken);
 					// now extract the realm
 //					JSONObject jsonObj = null;
 //					String decodedJson = null;
@@ -85,10 +85,10 @@ public class PathBasedKeycloakConfigResolver implements KeycloakConfigResolver {
 					username = gennyToken.getUsername();
 					key = gennyToken.getRealm()+".json";
 					realm = gennyToken.getRealm();
-					log.info("key="+key);
+					log.debug("key="+key);
 					
 				} else {
-					log.info("request.getURI()="+request.getURI());
+					log.debug("request.getURI()="+request.getURI());
 					if (request.getURI().equals("http://localhost:8080/version")) {
 						realm = GennySettings.mainrealm;
 					} else {
